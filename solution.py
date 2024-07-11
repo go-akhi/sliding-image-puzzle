edafbc048adcc810c761134538119fd80947f699
go-akhi <go.akhilesh@outlook.com>
2024-07-11 11:06:40 +0900

    made puzzleboard and populated tiles

---
+++ solution.py
@@ -0,0 +1,35 @@
+import networkx as nx
+#import matplotlib.pyplot as plt
+from pyvis.network import Network
+
+#the pos attribute ranges from 1 to 9 and will be the corresponding 
+#node for the puzzle piece
+
+#make the puzzleboard base
+def makeBoard():
+    Gr=nx.grid_2d_graph(3,3)
+    mapping={}
+    for x,y in Gr.nodes:
+        mapping[(x,y)]=((3*int(x))+int(y)+1)
+        Gr=nx.relabel_nodes(Gr,mapping)
+    return Gr
+
+def populateBoard(layout,G):
+    layout=layout.split(" ")
+    for x in range(len(layout)):
+        G.nodes[x+1]["title"]=layout[x]
+
+G = makeBoard()
+
+layout = str(input("Enter the puzzle layout in a single line, space delimited, LTR, 0 for empty tile : "))
+
+populateBoard(layout,G)
+
+print(G.nodes(data=True))
+
+#nt = Network(directed=False)
+#nt.from_nx(G)
+#nt.show("Puzzleboard.html",notebook=False)
+
+#nx.draw(G,with_labels=True)
+#plt.show()
